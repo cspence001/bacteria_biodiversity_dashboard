d964013d775443a372523ac6c7fdaba5c510926a
Ciara Spencer <ciara.spencer314@gmail.com>
2021-03-10 16:15:36 -0500

    file paths

--- static/js/app.js
+++ static/js/app.js
@@ -2,7 +2,7 @@
 
 function plots(id) {
     d3.json("samples.json").then((data) => {
-        // var samples = data.samples.filter(d => d.id ===id)[0];
+        // var samples = data.samples;
         // var samples_ids = samples.filter(d => d.id ===id)[0];
         console.log(data);
         // console.log(samples_ids);
